3bc8ccdbf2609c160b7ec9962407e8a984d8c459
ETorresSacha <eriktorressacha@gmail.com>
2023-09-15 13:45:21 -0500

    Nav

---
+++ src/componentes/landigPage/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../nav/Nav";
+import Footer from "../footer/Footer";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Nav />
+      <div>LandingPage</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
