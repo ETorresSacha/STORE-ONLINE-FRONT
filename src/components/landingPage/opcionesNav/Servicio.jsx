a7c372d7d2ba9a1405e27844a67a9244bca2d015
ETorresSacha <eriktorressacha@gmail.com>
2023-09-25 18:36:51 -0500

    landing/servicio

--- src/components/landingPage/opcionesNav/Servicio.jsx
+++ src/components/landingPage/opcionesNav/Servicio.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "./servicio.css";
 
 const Servicio = () => {
-  return <div>Servicio</div>;
+  return (
+    <div className="conteiner-servicio">
+      <h1> MIS SERVICIOS</h1>
+      <div className="conteiner-car-servicio">
+        <div className="car-servicio">
+          <img src="../../../../image/store.jpg" />
+          <h4>hola</h4>
+        </div>
+        <div className="car-servicio">
+          <img src="../../../../image/store.jpg" />
+          <h4>hola</h4>
+        </div>
+        <div className="car-servicio">
+          <img src="../../../../image/store.jpg" />
+          <h4>hola</h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Servicio;
