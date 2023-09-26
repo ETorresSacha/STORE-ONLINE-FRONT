34d4ef6117acbb4debe260c377006cf939889598
ETorresSacha <eriktorressacha@gmail.com>
2023-09-26 14:09:04 -0500

    sobreNosotros

---
+++ src/components/landingPage/opcionesNav/SobreNosotros.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const SobreNosotros = () => {
+  return (
+    <div>
+      <div>
+        <h2>Nosotros</h2>
+        <p>
+          Bienvenido a STORE, tu destino en línea para productos de alta calidad
+          y servicio excepcional. Queremos que conozcas un poco más sobre
+          nosotros y lo que representamos.
+        </p>
+      </div>
+      <div>
+        <h2>Nuestra Misión</h2>
+        <p>
+          En STORE, nuestra misión es simple pero poderosa: ofrecer a nuestros
+          clientes productos de alta calidad que mejoren sus vidas. Estamos
+          comprometidos en proporcionar una selección cuidadosamente curada de
+          productos que inspiren, solucionen problemas y aporten alegría a tu
+          día a día. Trabajamos incansablemente para ser tu fuente confiable de
+          productos excepcionales y un servicio al cliente incomparable.
+        </p>
+      </div>
+      <div>
+        <h2>Nuestros Valores</h2>
+        <h3>
+          En el corazón de todo lo que hacemos se encuentran nuestros valores
+          fundamentales:
+        </h3>
+        <span>
+          <li>
+            Calidad : Creemos en la excelencia y nos esforzamos por ofrecer
+            productos que cumplan y superen sus expectativas.
+          </li>
+          <li>
+            Integridad : Operamos con transparencia y honestidad en todas
+            nuestras interacciones.
+          </li>
+          <li>
+            Pasión : Estamos apasionados por lo que hacemos y por las
+            comunidades a las que servimos.
+          </li>
+          <li>
+            Innovación : Buscamos constantemente nuevas formas de mejorar y
+            crecer.
+          </li>
+          <li>
+            Compromiso con el Cliente : Tu satisfacción es nuestra máxima
+            prioridad.
+          </li>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default SobreNosotros;
