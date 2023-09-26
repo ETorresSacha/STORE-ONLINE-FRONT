e3bcc3baf6e92540896b0db47394f82398fd91d0
ETorresSacha <eriktorressacha@gmail.com>
2023-09-25 21:43:55 -0500

    servicios

--- src/components/landingPage/opcionesNav/Servicio.jsx
+++ src/components/landingPage/opcionesNav/Servicio.jsx
@@ -2,12 +2,34 @@ import React from "react";
 import "./servicio.css";
 
 const Servicio = () => {
-  const serviciosStore = [{}, {}, {}];
+  const serviciosStore = [
+    {
+      tipo: "Ropas",
+      imagen: "../../../../image/clothes.jpg",
+    },
+    {
+      tipo: "Accesorios",
+      imagen: "../../../../image/accesorios.jpg",
+    },
+    {
+      tipo: "Joyas",
+      imagen: "../../../../image/joyas.jpg",
+    },
+  ];
+  console.log(serviciosStore);
   return (
     <div className="conteiner-servicio">
       <h1> MIS SERVICIOS</h1>
       <div className="conteiner-car-servicio">
-        <div className="car-servicio">
+        {serviciosStore.map((element, index) => {
+          return (
+            <div className="car-servicio">
+              <img src={element.imagen} />
+              <h4>{element.tipo}</h4>
+            </div>
+          );
+        })}
+        {/* <div className="car-servicio">
           <img src="../../../../image/store.jpg" />
           <h4>hola</h4>
         </div>
@@ -18,7 +40,7 @@ const Servicio = () => {
         <div className="car-servicio">
           <img src="../../../../image/store.jpg" />
           <h4>hola</h4>
-        </div>
+        </div> */}
       </div>
     </div>
   );
