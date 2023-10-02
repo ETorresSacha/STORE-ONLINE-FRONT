406c6ca6c9453f0b83fbb1c7415cbfc5899d0a1f
ETorresSacha <eriktorressacha@gmail.com>
2023-10-02 17:30:32 -0500

    estilos/nosotros

--- src/components/landingPage/opcionesNav/SobreNosotros.jsx
+++ src/components/landingPage/opcionesNav/SobreNosotros.jsx
@@ -146,18 +146,37 @@ const SobreNosotros = () => {
 
   const MostrarContenido = () => {
     const result = description.filter((ele) => ele.title === estado);
-    console.log(result);
+
     return (
-      <div>
-        <h2>{result[0]?.title}</h2>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          padding: "25px",
+          textAlign: "justify",
+          alignItems: "center",
+        }}
+      >
+        <h2
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "35px",
+          }}
+        >
+          {result[0]?.title}
+        </h2>
         <p>{result[0]?.contexto}</p>
         <div>
           {!result[0]?.items
             ? ""
             : result[0]?.items.map((element) => {
                 return (
-                  <li style={{ border: "solid", flexWrap: "wrap" }}>
-                    <h3>{element.valor}:</h3> <h5>{element.texto}</h5>
+                  <li style={{ display: "flex", margin: "5px", width: "100%" }}>
+                    <h3 style={{ fontSize: "16px" }}>{element.valor}:</h3>
+                    {element.texto}
                   </li>
                 );
               })}
@@ -167,101 +186,54 @@ const SobreNosotros = () => {
   };
 
   return (
-    <div>
-      {/* <img
-        src="../../../../image/portada-nosotros.jpg"
-        style={{ width: "100%", backdropFilter: "blur(100px)" }}
-      /> */}
-      <Box
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          width: "100%",
-        }}
-      >
-        {images.map((image) => (
-          <ImageButton
-            focusRipple
-            key={image.title}
-            style={{
-              width: image.width,
-            }}
-          >
-            <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
-            <ImageBackdrop className="MuiImageBackdrop-root" />
-            <Image>
-              <Typography
-                component="span"
-                variant="subtitle1"
-                color="inherit"
-                sx={{
-                  position: "relative",
-                  p: 4,
-                  pt: 2,
-                  pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
-                }}
-                onClick={() => handleClick(image.title)}
-              >
-                {image.title}
-                <ImageMarked className="MuiImageMarked-root" />
-              </Typography>
-            </Image>
-          </ImageButton>
-        ))}
-      </Box>
-      <MostrarContenido />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        justifyContent: "space-around",
+      }}
+    >
+      <div>
+        <Box sx={{ width: "100%" }}>
+          {images.map((image) => (
+            <ImageButton
+              focusRipple
+              key={image.title}
+              style={{
+                width: image.width,
+              }}
+            >
+              <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+              <ImageBackdrop className="MuiImageBackdrop-root" />
+              <Image>
+                <Typography
+                  component="span"
+                  variant="subtitle1"
+                  color="inherit"
+                  sx={{
+                    position: "relative",
+                    p: 4,
+                    pt: 2,
+                    pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+                  }}
+                  onClick={() => handleClick(image.title)}
+                >
+                  {image.title}
+                  <ImageMarked className="MuiImageMarked-root" />
+                </Typography>
+              </Image>
+            </ImageButton>
+          ))}
+        </Box>
+      </div>
 
-      {/* <div>
-        <Button disabled={false}> hola</Button>
-        <h2>Nosotros</h2>
-        <p>
-          Bienvenido a STORE, tu destino en línea para productos de alta calidad
-          y servicio excepcional. Queremos que conozcas un poco más sobre
-          nosotros y lo que representamos.
-        </p>
-      </div>
       <div>
-        <h2>Nuestra Misión</h2>
-        <p>
-          En STORE, nuestra misión es simple pero poderosa: ofrecer a nuestros
-          clientes productos de alta calidad que mejoren sus vidas. Estamos
-          comprometidos en proporcionar una selección cuidadosamente curada de
-          productos que inspiren, solucionen problemas y aporten alegría a tu
-          día a día. Trabajamos incansablemente para ser tu fuente confiable de
-          productos excepcionales y un servicio al cliente incomparable.
-        </p>
+        <MostrarContenido />
       </div>
-      <div>
-        <h2>Nuestros Valores</h2>
-        <h3>
-          En el corazón de todo lo que hacemos se encuentran nuestros valores
-          fundamentales:
-        </h3>
-        <span>
-          <li>
-            Calidad : Creemos en la excelencia y nos esforzamos por ofrecer
-            productos que cumplan y superen sus expectativas.
-          </li>
-          <li>
-            Integridad : Operamos con transparencia y honestidad en todas
-            nuestras interacciones.
-          </li>
-          <li>
-            Pasión : Estamos apasionados por lo que hacemos y por las
-            comunidades a las que servimos.
-          </li>
-          <li>
-            Innovación : Buscamos constantemente nuevas formas de mejorar y
-            crecer.
-          </li>
-          <li>
-            Compromiso con el Cliente : Tu satisfacción es nuestra máxima
-            prioridad.
-          </li>
-        </span>
-      </div> */}
     </div>
   );
 };
 
 export default SobreNosotros;
+//!FALTA MEJORAR EN ESTILOS
